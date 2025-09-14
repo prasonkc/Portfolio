7b630904b7b8e006dfe38299ac2f68c2178ae630
Prason <prasonkc07@gmail.com>
2025-09-14 23:45:40 +0545

    Description Design

--- my-portfolio/src/components/Protrait.jsx
+++ my-portfolio/src/components/Protrait.jsx
@@ -11,8 +11,27 @@ const Portrait = () => {
           className="w-full h-full object-cover rounded-[inherit] opacity-90"
         />
       </div>
-      <div className="desc text-white w-[60%] ">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt porro, nesciunt excepturi reprehenderit totam libero praesentium dolore eius, omnis ea in atque suscipit assumenda facilis sunt. Quam commodi nesciunt iusto facilis sapiente, expedita perferendis ratione fugit suscipit facere repellat assumenda exercitationem corrupti cupiditate totam molestiae, iste vero corporis quia eaque illo nam reiciendis. Eligendi quas quidem, dolores accusamus commodi voluptas autem praesentium. Alias accusantium nam labore rerum doloribus ipsam magnam optio odit commodi illum?
+
+      {/* Description */}
+      <div
+        className="desc text-cyan-200 w-[60%] text-3xl"
+        style={{
+          fontFamily: "'Cyberfont', monospace",
+          color: "#00ffd0",
+          textShadow: `
+          1px 1px 2px #00ffe5,
+          2px 2px 4px rgba(0,255,240,0.3)
+        `,
+        }}
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt porro,
+        nesciunt excepturi reprehenderit totam libero praesentium dolore eius,
+        omnis ea in atque suscipit assumenda facilis sunt. Quam commodi nesciunt
+        iusto facilis sapiente, expedita perferendis ratione fugit suscipit
+        facere repellat assumenda exercitationem corrupti cupiditate totam
+        molestiae, iste vero corporis quia eaque illo nam reiciendis. Eligendi
+        quas quidem, dolores accusamus commodi voluptas autem praesentium. Alias
+        accusantium na
       </div>
     </div>
   );
